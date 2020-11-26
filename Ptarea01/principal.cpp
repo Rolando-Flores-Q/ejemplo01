b2ba0d61a434d327feeabede1d34e959ba892adc
Rolando Flores Quispe <74267964+Rolando-Flores-Q@users.noreply.github.com>
2020-11-26 05:48:41 -0600

    Add files via upload

---
+++ Ptarea01/principal.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	cout << "    DATOS PERSONALES" << endl;
+	cout << "    NOMBRE: ROLANDO FLORES QUISPE" << endl;
+	cout << "    CARRERA: ING DE SISTEMAS" << endl;
+	cout << "    CU: 35-4919" << endl;
+	cout << "    EDAD: 20" << endl;
+	system("pause");
+	return 0;
+}
